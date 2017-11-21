2484ae3b6dc2b5ac8953e90f62ace7aa516d06e7
juarrios <juarrios@gmail.com>
2017-11-21 10:27:36 -0700

    Initial Commit

---
+++ XcodeGitExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeGitExample
+//
+//  Created by Juan Barrios on 11/21/17.
+//  Copyright © 2017 Juan Barrios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
